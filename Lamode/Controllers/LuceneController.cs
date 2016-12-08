9b1c2723f97e0a5ba5657d3542ef4b13c18c1751
parsa <f_lamee2002@yahoo.com>
2016-12-07 22:48:53 -0800

    Lucine.net and Lucene.Net.Contrib instlled

---
+++ Lamode/Controllers/LuceneController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lamode.Controllers
+{
+    public class LuceneController : Controller
+    {
+        // GET: Lucene
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
