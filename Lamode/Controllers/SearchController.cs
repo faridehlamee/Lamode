7c15ac545b025a51b3fad9cf949c9fd6358b0e30
parsa <f_lamee2002@yahoo.com>
2017-02-28 00:37:51 -0800

    sort user base on user name and email done!

---
+++ Lamode/Controllers/SearchController.cs
@@ -0,0 +1,41 @@
+﻿using Lamode.Repositiries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lamode.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+
+        
+        public ActionResult Index(string sortOrder)
+        {
+            LamodeEntities context = new LamodeEntities();
+ 
+            UserRepository userRepo = new UserRepository();
+            IEnumerable<AspNetUser> users = userRepo.GetUsers(sortOrder);
+
+            // Store current sort filter parameter.
+            ViewBag.CurrentSort = sortOrder;
+
+            // Provide toggle option for name sort.
+            if (String.IsNullOrEmpty(sortOrder))
+                ViewBag.NameSortParm = UserRepository.LASTNAME;
+            else
+                ViewBag.NameSortParm = "";
+
+            // Provide toggle  optionfor date sort.
+            if (sortOrder == UserRepository.EMAIL)
+                ViewBag.DateSortParm = UserRepository.EMAIL;
+            else
+                ViewBag.DateSortParm = UserRepository.LASTNAME;
+
+            return View(users);
+        }
+
+    }
+}
---
+++ Lamode/Repositiries/UserRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lamode.Repositiries
+{
+    class UserRepository
+    {
+        public const string LASTNAME = "LastName";
+        public const string EMAIL = "Date";
+        
+
+        IEnumerable<AspNetUser> SortUsers(IEnumerable<AspNetUser> users, string sortOrder)
+        {
+            switch (sortOrder)
+            {
+                case LASTNAME:
+                    users = users.OrderByDescending(s => s.UserName);
+                    break;
+                case EMAIL:
+                    users = users.OrderBy(s => s.Email);
+                    break;
+               
+                default:
+                    users = users.OrderBy(s => s.UserName);
+                    break;
+            }
+            return users;
+        }
+
+        public IEnumerable<AspNetUser> GetUsers(string sortOrder)
+        {
+            LamodeEntities context = new LamodeEntities();
+            IEnumerable<AspNetUser> users = from s in context.AspNetUsers
+                                            select s;
+            users = SortUsers(users, sortOrder);
+            return users;
+        }
+
+    }
+}
